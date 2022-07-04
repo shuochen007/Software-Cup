22065596a77fe7cf0bab0ca0013c986ce592d444
shuochen007 <1264133835@qq.com>
2022-07-04 17:21:15 +0800

    first commit

---
+++ vue01/src/views/test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="edit" style="padding: 0;margin: 0;">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <div id="all">
+          <div id="inputArea">
+            <textarea dir="auto" placeholder="请输入文章内容" style="height: 300px;width: 600px;background-color: #DCDBDF"></textarea>
+
+          </div>
+          <div id="arrowArea">
+
+          </div>
+          <div id="buttonArea">
+            <el-row><el-button type="primary" plain>主要按钮</el-button></el-row>
+            <el-row><el-button type="success" plain>成功按钮</el-button></el-row>
+          </div>
+        </div>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      input: '',
+    }
+  },
+}
+</script>
+
+<style>
+
+textarea{
+  resize:none;
+}
+
+</style>
